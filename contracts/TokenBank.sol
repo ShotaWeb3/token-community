74909a21fd0a1de7c572947a0c078ef1b8393712
shota.inoue <shota.inoue@porters.jp>
2023-09-04 18:24:31 +0900

    TokenBank:トークン移転の実装

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -69,4 +69,26 @@ contract TokenBank {
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
+
+    /// @dev トークンを移転
+    function transfer(address to, uint256 amount) public {
+        address from = msg.sender;
+        _transfer(from, to, amount);
+    }
+
+    /// @dev 移転処理
+    function _transfer(
+        address from, 
+        address to,
+        uint256 amount
+    ) internal {
+        require(to != address(0), "Zero address cannot specified for 'to'");
+        uint256 fromBalance = _balances[from];
+
+        require(fromBalance >= amount, "Insufficient balance!");
+        _balances[from] = fromBalance - amount;
+        _balances[to]   += amount;
+        emit TokenTransfer(from, to, amount);
+    }
+
 }
--- test/TokenBank.js
+++ test/TokenBank.js
@@ -7,9 +7,12 @@ describe("TokenBankコントラクト", () => {
   const name = "Token";
   const symbol = "Tbk";
   let owner;
+  let addr1;
+  let addr2;
+  const zeroAddress = "0x0000000000000000000000000000000000000000";
 
   beforeEach(async () => {
-    [owner] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
     TokenBank = await ethers.getContractFactory("TokenBank");
     tokenBank = await TokenBank.deploy(name, symbol);
     await tokenBank.deployed(); 
@@ -25,6 +28,35 @@ describe("TokenBankコントラクト", () => {
     it("ownerに総額が割り当てられるべき", async () => {
       const ownerBalance = await tokenBank.balanceOf(owner.address);
       expect(await tokenBank.totalSupply()).to.equal(ownerBalance);
-    }); 
+    });
+  });
+  describe("アドレス間トランザクション", () => {
+    beforeEach(async () => {
+      await tokenBank.transfer(addr1.address, 500);
+    });
+    it("トークン移転がされるべき", async () => {
+      const startAddr1Balance = await tokenBank.balanceOf(addr1.address);
+      const startAddr2Balance = await tokenBank.balanceOf(addr2.address);
+
+      await tokenBank.connect(addr1).transfer(addr2.address, 100);
+
+      const endAddr1Balance = await tokenBank.balanceOf(addr1.address);
+      const endAddr2Balance = await tokenBank.balanceOf(addr2.address);
+
+      expect(endAddr1Balance).to.equal(startAddr1Balance.sub(100));
+      expect(endAddr2Balance).to.equal(startAddr2Balance.add(100));
+    });
+    it("ゼロアドレスの移転は失敗するべき", async () => {
+      await expect(tokenBank.transfer(zeroAddress, 100))
+      .to.be.revertedWith("Zero address cannot specified for 'to'");
+    });
+    it("残高不足の場合は移転に失敗するべき", async () => {
+      await expect(tokenBank.connect(addr1).transfer(addr2.address, 510))
+      .to.be.revertedWith("Insufficient balance!");
+    });
+    it("移転後は'TokenTransfer'イベントが発行されるべき", async () => {
+      expect(await tokenBank.connect(addr1).transfer(addr2.address, 100))
+      .emit(tokenBank, "TokenTransfer").withArgs(addr1.address, addr2.address, 100);
+    });
   });
 });
